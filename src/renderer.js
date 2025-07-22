2d7bae6f2a0c790590e61c20548b6b322fe41ee7
Victor-Hck <victor.profissional3592@gmail.com>
2025-07-22 10:21:14 -0300

    modified renderer.js

--- src/renderer.js
+++ src/renderer.js
@@ -1,12 +1,12 @@
 function login() {
-  const username = document.querySelector('input[type="text"]').value;
-  const password = document.querySelector('input[type="password"]').value;
+const username = document.querySelector('input[type="text"]').value;
+const password = document.querySelector('input[type="password"]').value;
 
-  if (username === 'admin' && password === '123') {
+if (username === 'admin' && password === '123') {
     alert('Login bem-sucedido!');
-  } else {
+} else {
     alert('Usuário ou senha inválidos.');
-  }
+}
 }
 
 console.log("Renderer.js carregado.");
