5822afd5590ab8ad9435ecea71bb25fb3638cec3
Victor-Hck <victor.profissional3592@gmail.com>
2025-07-22 10:12:11 -0300

    created login screen

---
+++ src/renderer.js
@@ -0,0 +1,12 @@
+function login() {
+  const username = document.querySelector('input[type="text"]').value;
+  const password = document.querySelector('input[type="password"]').value;
+
+  if (username === 'admin' && password === '123') {
+    alert('Login bem-sucedido!');
+  } else {
+    alert('Usuário ou senha inválidos.');
+  }
+}
+
+console.log("Renderer.js carregado.");
---
+++ src/script.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Recarrega a janela automaticamente quando arquivos mudam
+require('electron-reload')(__dirname, {
+    electron: require(`${__dirname}/../node_modules/electron`)
+});
+
+function createWindow () {
+    const win = new BrowserWindow({
+        width: 1280,
+        height: 720,
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false, // necessário para usar require no renderer.js
+    }
+});
+
+win.loadFile(path.join(__dirname, 'index.html'));
+
+//Remove o menu padrão
+win.removeMenu();
+
+// Abre o DevTools automaticamente
+// win.webContents.openDevTools();
+}
+
+app.whenReady().then(createWindow);
