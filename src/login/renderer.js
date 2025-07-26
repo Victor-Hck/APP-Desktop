9a8c07cf151612e34c534bd2ecf81de0d4b18e7e
Victor-Hck <victor.profissional3592@gmail.com>
2025-07-26 12:50:46 -0300

    broken code

--- src/login/renderer.js
+++ src/login/renderer.js
@@ -1,13 +1,37 @@
 function login() {
-const username = document.getElementById('usuario').value;
-const password = document.getElementById('senha').value;
+    const username = document.getElementById('usuario').value.trim();
+    const password = document.getElementById('senha').value.trim();
 
-if (username === 'admin' && password === '123') {
-    window.location.href = '../mainpage/dashboard.html'; // Troca de página!
-    // alert('Login bem-sucedido!');
-} else {
-    alert('Usuário ou senha inválidos.');
-}
+    if (username === "" || password === "") {
+        alert("Usuário e senha incorretos");
+        return; // PARA AQUI e não executa nada mais
+    }
+    window.location.href = "../mainpage/dashboard.html"; // Troca de página!
 }
 
 console.log("Renderer.js carregado.");
+
+const video = document.getElementById("meuVideo");
+const icon = document.getElementById("volumeIcon")
+
+function toggleMute() {
+    video.muted = !video.muted;
+    if (video.mute) {
+        icon.innerHTML = `
+        <path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v8.06c1.48-.74 2.5-2.26 2.5-4.03z"/>
+        <path d="M3 10v4h4l5 5V5l-5 5H3z"/>
+        <line x1="1" y1="1" x2="23" y2="23" stroke="white" stroke-width="2"/>
+        `;
+    } else {
+        icon.innerHTML = `<path d="M3 10v4h4l5 5V5l-5 5H3z"/>`;
+    }
+}
+
+function mudarVolume(valor) {
+    video.volume = valor;
+    if (video.volume === 0) {
+        video.muted = true;
+    } else {
+        video.muted = false;
+    }
+}
